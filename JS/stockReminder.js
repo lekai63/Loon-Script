323015cfe1d548df73e2fb9c7f4adf1b4f83ac25
lekai63 <lekai63@gmail.com>
2021-02-16 18:30:39 +0800

    Update stockReminder.js

--- JS/stockReminder.js
+++ JS/stockReminder.js
@@ -5,15 +5,16 @@ function getCurrentPrice(stockCode) {
        var StatusCode,ResponseHeaders;
        StatusCode = response.status;
        ResponseHeaders = response.headers;
-       console.log("response:\n",response);
-       console.log("StatusCode:\n",StatusCode);//400
-       console.log("ResponseHeaders:\n",ResponseHeaders);//Object
-       console.log("error:\n",error);
-       console.log("data:\n",data);
+       console.log("response:\n"+response);
+       console.log("StatusCode:\n"+StatusCode);//400
+       console.log("ResponseHeaders:\n"+ResponseHeaders);//Object
+       console.log("error:\n"+error);
+       console.log("data:\n"+data);
        // console.log("dataparse:\n",Json.parse(data));//if data is Json String
        //Loon支持使用Console.log输出调试信息
       var parts = data.split('\"');
-      var elements = (parts[1]).split(",");
+      var part = parts[1];
+      var elements = part.split(",");
       var Title = elements[0]; // 证券名称
       var SubTitle = "当前价"+elements[3]; // 当前价格
       var Content = "买一"+elements[6]+"\n"+"时间"+elements[31]
