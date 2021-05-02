2dafe3eb0b4388152bae50b3d6d021ddc02410fe
lekai <lekai63@gmail.com>
2021-05-02 19:54:58 +0800

    update

--- JS/dida.js
+++ JS/dida.js
@@ -3,5 +3,5 @@ var obj = JSON.parse(body);
 obj["proEndDate"]="2099-01-01T00:00:00.000+0000"
 obj["needSubscribe"]=false;
 obj["pro"]=true;
-body = JSON.stringify(obj)
+body = JSON.stringify(obj);
 $done({body});
