e2feb4012813084667c3514e8d1ad67bb7bf963f
lekai <lekai63@gmail.com>
2021-05-02 19:58:20 +0800

    log out

--- JS/dida.js
+++ JS/dida.js
@@ -1,7 +1,9 @@
 var body = $response.body;
 var obj = JSON.parse(body);
-obj["proEndDate"]="2099-01-01T00:00:00.000+0000"
+console.log("before_obj:",obj);
+obj["proEndDate"]="2025-01-01T00:00:00.000+0000";
 obj["needSubscribe"]=false;
 obj["pro"]=true;
 body = JSON.stringify(obj);
+console.log("after_body:",body);
 $done({body});
